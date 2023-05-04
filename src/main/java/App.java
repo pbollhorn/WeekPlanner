6a1d46d39be762f77695aca1cbe2dc78e30b82cf
Peter Bollhorn <pbollhorn@gmail.com>
2023-05-04 21:19:43 +0200

    commiting version 0.3

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,114 +11,119 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class App extends HttpServlet {
+public class App extends HttpServlet
+{
 
 	private static final long serialVersionUID = 1L;
 
 	private static ArrayList<ArrayList<String>> weekPlan;
 
-	public App() {
+	public App()
+	{
 
 		// For debugging purposes
 		System.out.println("Running constructor for App class");
 
 		// Initialize empty weekPlan and read in data from database
 		weekPlan = new ArrayList<ArrayList<String>>();
-		for (int day = 0; day < 7; day++) {
+		for (int day = 0; day < 7; day++)
+		{
 			weekPlan.add(new ArrayList<String>());
 			databaseReadDay(dayInt2Str(day));
 		}
 
 	}
 
-	
-	private void databaseReadDay(String day) {
-		
+	private void databaseReadDay(String day)
+	{
+
 		day = day.toLowerCase();
 		ArrayList<String> dayPlan = weekPlan.get(dayStr2Int(day));
 		dayPlan.clear(); // TODO: Is this line necessary?
-		
+
 		String jdbcURL = "jdbc:postgresql://localhost:5432/WeekPlanner";
 		String username = "postgres";
 		String password = "crawler";
-		
-		try {
-			
+
+		try
+		{
+
 			// Establish connection
 			Class.forName("org.postgresql.Driver");
 			Connection connection = DriverManager.getConnection(jdbcURL, username, password);
 			System.out.println("Connected to PostgreSQL");
-			
+
 			// Read day plan from appropriate table
 			String sql = "SELECT * FROM " + day + " ORDER BY task_order ASC";
 			System.out.println(sql);
 			Statement statement = connection.createStatement();
 			ResultSet result = statement.executeQuery(sql);
-			while(result.next() ) {
+			while (result.next())
+			{
 				int id = result.getInt("id");
 				String taskDescription = result.getString("task_description");
-				if( taskDescription != null) {
+				if (taskDescription != null)
+				{
 					dayPlan.add(taskDescription);
 					System.out.println("id=" + id + " task_description=" + taskDescription);
 				}
 
 			}
-					
+
 			// Close connection
 			connection.close();
-		
-		} catch(Exception e) {
+
+		} catch (Exception e)
+		{
 			System.out.println("Error in connecting to PostgreSQL server");
 			e.printStackTrace();
-		}	
-		
-		
+		}
+
 	}
-	
-	
-	
-	private void databaseUpdateDay(String day) {
-		
+
+	private void databaseUpdateDay(String day)
+	{
+
 		day = day.toLowerCase();
 		ArrayList<String> dayPlan = weekPlan.get(dayStr2Int(day));
-		
+
 		String jdbcURL = "jdbc:postgresql://localhost:5432/WeekPlanner";
 		String username = "postgres";
 		String password = "crawler";
-		
-		try {
-			
+
+		try
+		{
+
 			// Establish connection
 			Class.forName("org.postgresql.Driver");
 			Connection connection = DriverManager.getConnection(jdbcURL, username, password);
 			System.out.println("Connected to PostgreSQL");
-			
-			
-			
+
 			// Loop over tasks and update appropriate table with the new day plan
-			for(int task=0;task<dayPlan.size();task++) {
-				String sql = "UPDATE " + day + " SET task_description = '" + dayPlan.get(task) +"' WHERE task_order = " + task;
+			for (int task = 0; task < dayPlan.size(); task++)
+			{
+				String sql = "UPDATE " + day + " SET task_description = '" + dayPlan.get(task) + "' WHERE task_order = "
+						+ task;
 				System.out.println(sql);
 				Statement statement = connection.createStatement();
 				statement.execute(sql);
 			}
-			
-					
+
 			// Close connection
 			connection.close();
-		
-		} catch(Exception e) {
+
+		} catch (Exception e)
+		{
 			System.out.println("Error in connecting to PostgreSQL server");
 			e.printStackTrace();
-		}	
-		
-		
+		}
+
 	}
-	
-	
-	
-	private String dayInt2Str(int day) {
-		switch (day) {
+
+	private String dayInt2Str(int day)
+	{
+		switch (day)
+		{
 		case 0:
 			return "Monday";
 		case 1:
@@ -137,11 +142,13 @@ public class App extends HttpServlet {
 		return "Error";
 	}
 
-	private int dayStr2Int(String day) {
-		
+	private int dayStr2Int(String day)
+	{
+
 		day = day.toLowerCase();
-		
-		switch (day) {
+
+		switch (day)
+		{
 		case "monday":
 			return 0;
 		case "tuesday":
@@ -160,8 +167,8 @@ public class App extends HttpServlet {
 		return -1;
 	}
 
-	private void planDay(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	private void planDay(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
 
 		String day = request.getParameter("day");
 		ArrayList<String> dayPlan = weekPlan.get(dayStr2Int(day));
@@ -174,12 +181,14 @@ public class App extends HttpServlet {
 		pw.println("<input type=\"hidden\" name=\"day\" value=\"" + day + "\" />");
 
 		int i = 0;
-		// Write input boxes with existing tasks from dayPlan 
-		for (; i < dayPlan.size(); i++) {
+		// Write input boxes with existing tasks from dayPlan
+		for (; i < dayPlan.size(); i++)
+		{
 			pw.println("<input type=\"text\" name=\"task" + i + "\" value=\"" + dayPlan.get(i) + "\"><br>");
 		}
 		// Write empty input boxes up to total number of 10
-		for (; i < 10; i++) {
+		for (; i < 10; i++)
+		{
 			pw.println("<input type=\"text\" name=\"task" + i + "\"><br>");
 		}
 
@@ -189,7 +198,8 @@ public class App extends HttpServlet {
 	}
 
 	private void submitDay(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+			throws ServletException, IOException
+	{
 
 		int day = dayStr2Int(request.getParameter("day"));
 
@@ -198,7 +208,8 @@ public class App extends HttpServlet {
 		dayPlan.clear();
 
 		// Add new content to dayPlan
-		for (int i = 0; i < 10; i++) {
+		for (int i = 0; i < 10; i++)
+		{
 			String task = request.getParameter("task" + i);
 
 			// if task is not an empty string, then add it to dayPlan
@@ -206,17 +217,15 @@ public class App extends HttpServlet {
 				dayPlan.add(task);
 		}
 
-		
 		// HERE I SHOULD UPDATE DATABASE
 		databaseUpdateDay(dayInt2Str(day));
-		
-		
+
 		// Go back to front page
 		response.sendRedirect("index.html");
 	}
 
-	private void showWeek(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	private void showWeek(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
 
 		PrintWriter pw = response.getWriter();
 		pw.println("<body>"); // this somehow gets the HTML code below to work
@@ -224,13 +233,15 @@ public class App extends HttpServlet {
 		pw.println("<a href=\"index.html\">Back to front page</a><br><br>");
 
 		// Loop through days in weekPlan
-		for (int day = 0; day < 7; day++) {
+		for (int day = 0; day < 7; day++)
+		{
 
 			pw.println("<b>" + dayInt2Str(day) + "</b><br>");
 			ArrayList<String> dayPlan = weekPlan.get(day);
 
 			// Loop through tasks in dayPlan and write to screen
-			for (int i = 0; i < dayPlan.size(); i++) {
+			for (int i = 0; i < dayPlan.size(); i++)
+			{
 				pw.println("- " + dayPlan.get(i) + "<br>");
 			}
 			pw.println("<br>");
@@ -238,18 +249,18 @@ public class App extends HttpServlet {
 		pw.close();
 	}
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
 
 		String action = request.getParameter("action");
-		
-		
-		// TODO: This avoid NullPointer exception if I accidentely run App.java instead of the project
-		if(action==null)
-			return;
-		
 
-		switch (action) {
+		// TODO: This avoid NullPointer exception if I accidentely run App.java instead
+		// of the project
+		if (action == null)
+			return;
+
+		switch (action)
+		{
 		case "planDay":
 			planDay(request, response);
 			break;
@@ -260,7 +271,6 @@ public class App extends HttpServlet {
 			showWeek(request, response);
 			break;
 		}
-		
 
 	}
 
