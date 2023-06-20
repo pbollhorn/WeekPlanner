ba1396548eee804396de7f7decd97e003f3e657a
Peter Bollhorn <pbollhorn@gmail.com>
2023-06-20 17:22:44 +0200

    many changes

--- src/main/webapp/js/manipulateDOM.js
+++ src/main/webapp/js/manipulateDOM.js
@@ -1,9 +1,14 @@
-var idLastClicked = null;
-var mainElement = document.querySelector("main");
-var myGreen = "rgb(144, 238, 144)";  // Not a web safe color
-var myYellow = "rgb(255,255,153)";
-//var myYellow = "rgb(240,240,240)";
-var myTransparent = "rgb(0,0,0,0)";
+let idLastClicked = null;
+const mainElement = document.querySelector("main");
+const myGreen = "rgb(144, 238, 144)";  // Not a web safe color
+const myYellow = "rgb(255,255,153)";
+const myGray = "rgb(200,200,200)";
+const myBlack = "rgb(0,0,0)";
+const myTransparent = "rgb(0,0,0,0)";
+
+let task_id_selected = null;
+
+let task_id = 0;
 
 
 // Object to keep track of the ids clicked
@@ -26,12 +31,15 @@ const idsClicked = {
 
 
 function deleteTask() {
-	
-	
-	const ls = idsClicked.lastClicked();
-	
-	if( ls ) {
-		document.getElementById(ls).remove();	
+		
+	if( task_id_selected !== null ) {
+		
+		const taskElement = document.getElementById(task_id_selected);
+		const otherTask = taskElement.nextElementSibling;
+		
+		selectTask(otherTask);
+		taskElement.remove();
+		
 	}
 
 }
@@ -39,11 +47,9 @@ function deleteTask() {
 
 function markTaskDone() {
 	
-	const ls = idsClicked.lastClicked();
-	
-	if( ls ) {
+	if( task_id_selected !== null ) {
 		
-		divElement = document.getElementById(ls);
+		divElement = document.getElementById(task_id_selected);
 		
 		const doneStatus = divElement.getAttribute("data-done-status");
 		
@@ -61,42 +67,148 @@ function markTaskDone() {
 }
 
 
-
-function clickTask(element) {
-
-	// Put transparent border around the task clicked last
-	const lastElement = document.getElementById(idsClicked.lastClicked());
-	if (lastElement) {
-		lastElement.querySelector("div").style.borderColor = myTransparent;
+function moveTaskUp() {
+	
+	taskElement = document.getElementById(task_id_selected);
+	const otherTaskElement = taskElement.previousElementSibling;
+	
+	if( otherTaskElement.innerText!=="Monday") {
+	
+		mainElement.insertBefore(taskElement,otherTaskElement);
+	
 	}
+	
+}
 
-	// Put black border around the task clicked now
-	element.querySelector("div").style.borderColor = "black";
 
-	// If task clicked now is also the task clicked last, then put focus on its inputElement
-	if (element.id === idsClicked.lastClicked()) {
-		const inputElement = element.querySelector("input");
-		const length = inputElement.value.length;
-		inputElement.setSelectionRange(length, length);
-		inputElement.focus();
-	}
+function moveTaskDown() {
+	
+	taskElement = document.getElementById(task_id_selected);
+	const otherTaskElement = taskElement.nextElementSibling;	
+	mainElement.insertBefore(otherTaskElement,taskElement);
+	
+}
 
-	// Register this task as the task clicked last
-	idsClicked.registerClick(element.id);
 
+// Add task
+function addTask() {
+		
+	taskElement = document.getElementById(task_id_selected);
+	let newTask = createTask("",false);	
+	taskElement.insertAdjacentElement("afterend",newTask);
+	
 }
 
 
 
 
+function selectTask(taskElement) {
+
+	if (taskElement.id === task_id_selected) {
+		
+		const inputElement = taskElement.querySelector("input");	
+		
+		if( inputElement !== document.activeElement ) {
+			
+			
+			const transparentDiv = taskElement.querySelector("div").nextElementSibling;
+			transparentDiv.style.display = "none";
+			
+			const length = inputElement.value.length;
+			inputElement.setSelectionRange(length, length);
+			inputElement.focus();	
+		}	
+	
+	} else {
+		
+		
+		
+		if(task_id_selected !== null) {
+			
+			// Unselect previous task element
+			const prevElement = document.getElementById(task_id_selected);
+			prevElement.querySelector("div").style.borderColor = myTransparent;
+			const transparentDiv = prevElement.querySelector("div").nextElementSibling;
+			transparentDiv.style.display = "block";
+			
+		}
+		
+		taskElement.querySelector("div").style.borderColor = myBlack;
+		
+		task_id_selected = taskElement.id;
+		
+	}
+		
+	//	
+	//	
+	////	// Put transparent border around previously selected sk
+	////	if( task_id_selected !== null {
+	////		const prevElement = document.getElementById(task_id_select);
+	////		prevElement.setAttribute("data-select-status",");
+	////		prevElement.querySelector("div").style.borderColor = myTranspare;	
+	//	}
+	//	
+	//	
+	////	// Select new task and put black border arounit
+	////	task_id_selected = taskElementd;
+	////	taskElement.setAttribute("data-select-status",");
+	//	taskElement.querySelector("div").style.borderColor = myBlac
+}
+
+
+
+// Create the special task div
+function createTask(description, done) {
+
+	// Create the task which is actually a div element, with two overlapping childs divs inside
+	const taskElement = document.createElement("div");
+	taskElement.className = "task";
+	taskElement.id = "task_" + task_id;
+	task_id++;
+	taskElement.onclick = function fun() { selectTask(this) };
+
+
+	// Create the two child divs on top of each other
+	taskElement.innerHTML = "<div><input type='text' value='" + description + "'></div><div></div>";
+
+	// Set background color of most backward div depending on if the task is done or not
+	if (done === true) {
+		taskElement.setAttribute("data-done-status", "true");
+		taskElement.querySelector("div").style.backgroundColor = myGreen;
+	}
+	else {
+		taskElement.setAttribute("data-done-status", "false");
+		taskElement.querySelector("div").style.backgroundColor = myYellow;
+	}
+
+	// Give the special task div a data-select-status of 0
+	taskElement.setAttribute("data-select-status", "0");
+
+
+	// IMPORTANT:
+	// onkeydown event: If user presses Enter, then input element should be blurred
+	taskElement.querySelector("input").onkeydown = function fun2(event) {
+		if (event.key === "Enter") {
+    		this.blur();
+    	}
+	};
+	
+	// IMPORTANT:
+	// onfocus event: If input elements get focus when its not suppose to,
+	// its because the user pressing Enter has forced it,
+	// and therefore we blur it
+	taskElement.querySelector("input").onfocus = function fun3() {
+		if( this.parentNode.parentNode.id !== task_id_selected ) { this.blur(); }
+	};
+
+	// Return the taskElement
+	return taskElement;
+
+}
 
 
 
 function buildView() {
-
-	let task_id = 0;
-
-	//const mainElement = document.querySelector("main");
 
 	// Loop through lists in plan
 	for (const list of plan.lists) {
@@ -108,31 +220,8 @@ function buildView() {
 
 		// Loop through tasks in list
 		for (const task of list.tasks) {
-
-			// Create the special task div
-			const divElement = document.createElement("div");
-			divElement.className = "task";
-			divElement.id = "task_" + task_id;
-			task_id++;
-			divElement.onclick = function fun() { clickTask(this) };
-			divElement.setAttribute("data-select-status","0");
-
-			// Create the two child divs on top of each other
-			divElement.innerHTML = "<div><input type='text' value='" + task.description + "'></div><div></div>";
-
-			// Set background color of most backward div depending on if the task is done or not
-			if (task.done === true) {
-				divElement.setAttribute("data-done-status","true");
-				divElement.querySelector("div").style.backgroundColor = myGreen;
-			}
-			else {
-				divElement.setAttribute("data-done-status","false");
-				divElement.querySelector("div").style.backgroundColor = myYellow;
-			}
-			
-			// Append the special task div to DOM
-			mainElement.appendChild(divElement);
-
+			const taskElement = createTask(task.description, task.done);
+			mainElement.appendChild(taskElement);
 		}
 
 	}
