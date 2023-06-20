dbde5bee70da58d91eaaece3a83bd467d0f68684
Peter Bollhorn <pbollhorn@gmail.com>
2023-06-20 17:56:43 +0200

    before using objects rather than ids to identify

--- src/main/webapp/js/manipulateDOM.js
+++ src/main/webapp/js/manipulateDOM.js
@@ -1,4 +1,3 @@
-let idLastClicked = null;
 const mainElement = document.querySelector("main");
 const myGreen = "rgb(144, 238, 144)";  // Not a web safe color
 const myYellow = "rgb(255,255,153)";
@@ -11,92 +10,85 @@ let task_id_selected = null;
 let task_id = 0;
 
 
-// Object to keep track of the ids clicked
-const idsClicked = {
-
-	// array to store the ids clicked
-	array: [null, null],
-
-	// return the last element of the array
-	lastClicked: function() {
-		return this.array[1];
-	},
-
-	// register latest click
-	registerClick: function(id) {
-		this.array[0] = this.array[1];
-		this.array[1] = id;
-	},
-};
-
 
 function deleteTask() {
-		
-	if( task_id_selected !== null ) {
-		
+
+	if (task_id_selected !== null) {
+
 		const taskElement = document.getElementById(task_id_selected);
-		const otherTask = taskElement.nextElementSibling;
-		
-		selectTask(otherTask);
+		const allTaskElements = document.querySelectorAll(".task");
+
+		// Loop through allTaskElements to find the next one after current taksElement
+		for (let i = 0; i < allTaskElements.length; i++) {
+
+			if (taskElement === allTaskElements[i]) {
+
+				selectTask(allTaskElements[i + 1]);
+			}
+
+
+		}
+
 		taskElement.remove();
-		
+
 	}
 
 }
 
 
 function markTaskDone() {
-	
-	if( task_id_selected !== null ) {
-		
+
+	if (task_id_selected !== null) {
+
 		divElement = document.getElementById(task_id_selected);
-		
+
 		const doneStatus = divElement.getAttribute("data-done-status");
-		
-		if( doneStatus === "false" ) {
-			divElement.setAttribute("data-done-status","true");
+
+		if (doneStatus === "false") {
+			divElement.setAttribute("data-done-status", "true");
 			divElement.querySelector("div").style.backgroundColor = myGreen;
 		}
 		else {
-			divElement.setAttribute("data-done-status","false");
+			divElement.setAttribute("data-done-status", "false");
 			divElement.querySelector("div").style.backgroundColor = myYellow;
 		}
-		
+
 	}
-	
+
 }
 
 
 function moveTaskUp() {
-	
+
 	taskElement = document.getElementById(task_id_selected);
 	const otherTaskElement = taskElement.previousElementSibling;
-	
-	if( otherTaskElement.innerText!=="Monday") {
-	
-		mainElement.insertBefore(taskElement,otherTaskElement);
-	
+
+	if (otherTaskElement.innerText !== "Monday") {
+
+		mainElement.insertBefore(taskElement, otherTaskElement);
+
 	}
-	
+
 }
 
 
 function moveTaskDown() {
-	
+
 	taskElement = document.getElementById(task_id_selected);
-	const otherTaskElement = taskElement.nextElementSibling;	
-	mainElement.insertBefore(otherTaskElement,taskElement);
-	
+	const otherTaskElement = taskElement.nextElementSibling;
+	mainElement.insertBefore(otherTaskElement, taskElement);
+
 }
 
 
 // Add task
 function addTask() {
-		
+
 	taskElement = document.getElementById(task_id_selected);
-	let newTask = createTask("",false);	
-	taskElement.insertAdjacentElement("afterend",newTask);
-	
+	let newTask = createTask("", false);
+	taskElement.insertAdjacentElement("afterend", newTask);
+	selectTask(newTask);
+
 }
 
 
@@ -105,54 +97,41 @@ function addTask() {
 function selectTask(taskElement) {
 
 	if (taskElement.id === task_id_selected) {
-		
-		const inputElement = taskElement.querySelector("input");	
-		
-		if( inputElement !== document.activeElement ) {
-			
-			
+
+		const inputElement = taskElement.querySelector("input");
+
+		if (inputElement !== document.activeElement) {
+
+
 			const transparentDiv = taskElement.querySelector("div").nextElementSibling;
 			transparentDiv.style.display = "none";
-			
+
 			const length = inputElement.value.length;
 			inputElement.setSelectionRange(length, length);
-			inputElement.focus();	
-		}	
-	
+			inputElement.focus();
+		}
+
 	} else {
-		
-		
-		
-		if(task_id_selected !== null) {
-			
+
+
+
+		if (task_id_selected !== null) {
+
 			// Unselect previous task element
 			const prevElement = document.getElementById(task_id_selected);
 			prevElement.querySelector("div").style.borderColor = myTransparent;
 			const transparentDiv = prevElement.querySelector("div").nextElementSibling;
 			transparentDiv.style.display = "block";
-			
+
 		}
-		
+
 		taskElement.querySelector("div").style.borderColor = myBlack;
-		
+
 		task_id_selected = taskElement.id;
-		
+
 	}
-		
-	//	
-	//	
-	////	// Put transparent border around previously selected sk
-	////	if( task_id_selected !== null {
-	////		const prevElement = document.getElementById(task_id_select);
-	////		prevElement.setAttribute("data-select-status",");
-	////		prevElement.querySelector("div").style.borderColor = myTranspare;	
-	//	}
-	//	
-	//	
-	////	// Select new task and put black border arounit
-	////	task_id_selected = taskElementd;
-	////	taskElement.setAttribute("data-select-status",");
-	//	taskElement.querySelector("div").style.borderColor = myBlac
+
+
 }
 
 
@@ -189,16 +168,16 @@ function createTask(description, done) {
 	// onkeydown event: If user presses Enter, then input element should be blurred
 	taskElement.querySelector("input").onkeydown = function fun2(event) {
 		if (event.key === "Enter") {
-    		this.blur();
-    	}
+			this.blur();
+		}
 	};
-	
+
 	// IMPORTANT:
 	// onfocus event: If input elements get focus when its not suppose to,
 	// its because the user pressing Enter has forced it,
 	// and therefore we blur it
 	taskElement.querySelector("input").onfocus = function fun3() {
-		if( this.parentNode.parentNode.id !== task_id_selected ) { this.blur(); }
+		if (this.parentNode.parentNode.id !== task_id_selected) { this.blur(); }
 	};
 
 	// Return the taskElement
