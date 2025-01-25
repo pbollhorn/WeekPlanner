a98a1e31ab9229995fa8d87d525311adbfcdc490
Peter Bollhorn <pbollhorn@gmail.com>
2025-01-25 11:40:05 +0100

    Added NOT NULL to sql script

--- src/main/resources/sql/WeekPlannerDB.sql
+++ src/main/resources/sql/WeekPlannerDB.sql
@@ -1,14 +1,16 @@
+-- Create public schema
 DROP SCHEMA IF EXISTS public CASCADE;
 CREATE SCHEMA public;
 CREATE TABLE public.user_data
 (
     user_id        SERIAL PRIMARY KEY,
-    username       TEXT UNIQUE,
-    password_hash  BYTEA,
-    salt           BYTEA,
-    encrypted_data BYTEA
+    username       TEXT UNIQUE NOT NULL,
+    password_hash  BYTEA NOT NULL,
+    salt           BYTEA NOT NULL,
+    encrypted_data BYTEA NOT NULL
 );
 
+-- Create test schema
 DROP SCHEMA IF EXISTS test CASCADE;
 CREATE SCHEMA test;
 CREATE TABLE test.user_data
