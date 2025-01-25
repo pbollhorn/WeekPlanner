e523ff2f882acf77839b191d1f3f753c342fbad8
Peter Bollhorn <pbollhorn@gmail.com>
2025-01-25 11:18:21 +0100

    Improved SQL script

--- src/main/resources/sql/WeekPlannerDB.sql
+++ src/main/resources/sql/WeekPlannerDB.sql
@@ -1,1 +1,17 @@
-CREATE TABLE user_data(user_id SERIAL PRIMARY KEY, username TEXT UNIQUE, password_hash BYTEA, salt BYTEA, encrypted_data BYTEA)
+DROP SCHEMA IF EXISTS public CASCADE;
+CREATE SCHEMA public;
+CREATE TABLE public.user_data
+(
+    user_id        SERIAL PRIMARY KEY,
+    username       TEXT UNIQUE,
+    password_hash  BYTEA,
+    salt           BYTEA,
+    encrypted_data BYTEA
+);
+
+DROP SCHEMA IF EXISTS test CASCADE;
+CREATE SCHEMA test;
+CREATE TABLE test.user_data
+(
+    LIKE public.user_data INCLUDING CONSTRAINTS
+);
